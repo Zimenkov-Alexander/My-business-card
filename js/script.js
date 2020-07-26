24826d9de52e27a457861b2eec136fbdf1cbc3b1
Alex <ZimenkovIUAI@gmail.com>
2020-07-26 18:49:20 +0300

    Доделал спойлер, подключил jquery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
-let burgerMain = document.querySelector('.burger');
-let pageHeader = document.querySelector('.page-header');
+let burgerMain = document.querySelector('.burger'),
+	pageHeader = document.querySelector('.page-header'),
+	// infoTitle = document.querySelectorAll('.info-title'),
+	// infoWrapper = document.querySelectorAll('.info-wrapper'),
+	pageExperienceWrapper = document.querySelector('.page-experience-wrapper');
 
 burgerMain.addEventListener('click', function(){
 	burgerMain.classList.toggle("active");
 	pageHeader.classList.toggle("visually-hidden");
+});
+
+$(document).ready(function() {
+	$('.info-title').click(function(event) {
+		if($('.experience__wrapper').hasClass('one')){
+			$('.info-title').not($(this)).removeClass('active');
+			$('.info-wrapper').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
 });
