c4c367774721cdb7d4218dea34bde1e67f2221da
Alex <ZimenkovIUAI@gmail.com>
2020-07-28 00:51:01 +0300

    Настроил SMTP

--- php/main.php
+++ php/main.php
@@ -13,13 +13,13 @@ $message = $_POST['message'];
 
 //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-$mail->isIMAP();                                      // Set mailer to use SMTP
-$mail->Host = 'imap.gmail.com';  																							// Specify main and backup SMTP servers
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  																							// Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'BusinessCard.Zimenkov@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'Creative55'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 993; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('BusinessCard.Zimenkov@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('Zimenkoviuai@gmail.com');     // Кому будет уходить письмо 
