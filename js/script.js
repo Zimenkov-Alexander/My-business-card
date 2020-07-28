5bcc627cba486e4a1b3e91646ef11d85e595f8c3
Alex <ZimenkovIUAI@gmail.com>
2020-07-29 02:15:00 +0300

    Добавил анимацию перехода по ссылкам

--- js/script.js
+++ js/script.js
@@ -85,3 +85,11 @@ $('.info-title').click(function(event) {
 	}
 	$(this).toggleClass('active').next().slideToggle(300);
 });
+
+
+$("#menu").on("click","a", function (event) {
+	event.preventDefault();
+	var id  = $(this).attr('href'),
+	top = $(id).offset().top;
+	$('body,html').animate({scrollTop: top}, 1000);
+});
